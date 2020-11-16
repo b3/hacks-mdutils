933fed834174432f9ac0c781484312334bc7fae2
Bruno BEAUFILS <bruno@boulgour.com>
2020-11-16 15:47:55 +0100

    filtres lua: prise en compte type beamer en plus de latex

--- etc/center-image.lua
+++ etc/center-image.lua
@@ -1,8 +1,9 @@
 --- center-image.lua – filter to center image on their line
 ---
 --- ideas from https://pandoc.org/lua-filters.html#center-images-in-latex-and-html-output
+--- TODO : rendre ça automatique pour les images seules sur leur ligne (sans être obligé d'ajouter \ 
 
-if FORMAT:match 'latex' then
+if FORMAT:match 'latex' or FORMAT:match 'beamer' then
   function Image (elem)
     if elem.classes:includes("center", 1) then
       return {
